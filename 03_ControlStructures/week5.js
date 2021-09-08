f2875a6d85e7fc1d491133443c3554d992768a4f
AthitiJames <69097916+AthitiJames@users.noreply.github.com>
2021-09-08 22:11:25 +0700

    week-5

--- 03_ControlStructures/week5.js
+++ 03_ControlStructures/week5.js
@@ -1,16 +1,22 @@
-triangle = {
-    base: 5,
-    high: 5,
-    area: function(){return 1/2*this.base*this.high}
-}
-
-console.log(triangle.area());
-
-// book = {
-//     id: 111,
-//     nameBook: ["Nani","fahh","mhai","prew"]
+// triangle = {
+//     base: 5,
+//     high: 5,
+//     area: function(){return 1/2*this.base*this.high}
 // }
 
-// console.log(book);
+// console.log(triangle.area());
 
-let thailand={lang: "Thai", state: ["bangkok", "chiangmai", "ubon", "phuket"]}
+// // book = {
+// //     id: 111,
+// //     nameBook: ["Nani","fahh","mhai","prew"]
+// // }
+
+// // console.log(book);
+
+// let thailand={lang: "Thai", state: ["bangkok", "chiangmai", "ubon", "phuket"]}
+
+let sum = 0;
+for(let i =0; i<=5;i++){
+    sum = sum+(2*i);
+    console.log(sum);
+}
